95ea2ca44db6ad33d1163434e151382261ba6501
Spiros Denaxas <s.denaxas@gmail.com>
2007-04-03 09:35:27 -0800

    import WebService-TWFY-API 0.02 from CPAN
    git-cpan-module:   WebService-TWFY-API
    git-cpan-version:  0.02
    git-cpan-authorid: SDEN
    git-cpan-file:     authors/id/S/SD/SDEN/WebService-TWFY-API-0.02.tar.gz

--- lib/WebService/TWFY/API.pm
+++ lib/WebService/TWFY/API.pm
@@ -9,7 +9,7 @@ use WebService::TWFY::Response ;
 use LWP::UserAgent ;
 
 our @ISA = qw( LWP::UserAgent ) ;
-our $VERSION = 0.01 ;
+our $VERSION = 0.02 ;
 
 =head1 NAME
 
@@ -17,7 +17,7 @@ WebService::TWFY::API - API interface for TheyWorkForYou.com
 
 =head1 VERSION
 
-Version 0.01  
+Version 0.02  
 
 =cut
 
